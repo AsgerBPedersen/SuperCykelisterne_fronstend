7e9dc029c2158ea26792d7130e2e7e2b0e998087
TooFatToPogo <44614683+TooFatToPogo@users.noreply.github.com>
2020-02-03 11:20:22 +0100

    add photo functionality

--- components/AddPhoto.js
+++ components/AddPhoto.js
@@ -1,33 +1,80 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import gql from "graphql-tag";
+import { Mutation } from "react-apollo";
+
+const CREATE_IMAGE = gql`
+  mutation CREATE_IMAGE($url: String!) {
+    createImage(url: $url) {
+      id
+    }
+  }
+`;
 
 class AddPhoto extends Component {
-    render() {
-        return (
-            <div>
-                <h4 className="text-center p-3">TILFØJ BILLEDE</h4>
-               
+  state = {};
 
-                <div className="form-group row">
-                    <div className="d-flex">
+  onChange = e => {
+    this.setState({
+      files: e.target.files
+    });
+  };
 
-                <label className="btn btn-outline-secondary text-nowrap m-0">
+  onSubmit = async (mutation) => {
+    const data = new FormData();
+    data.append("file", this.state.files[0]);
+    data.append("upload_preset", "SuperCykler");
+
+    const res = await fetch(
+      "https://api.cloudinary.com/v1_1/asge4899/image/upload",
+      {
+        method: "POST",
+        body: data
+      }
+    );
+    const img = await res.json();
+    console.log(img);
+
+    await mutation({
+        variables: {
+            url: img.secure_url
+        }
+    })
+  };
+
+  render() {
+    return (
+      <Mutation mutation={CREATE_IMAGE}>
+          {uploadImage => (
+              <div>
+              <h4 className="text-center p-3">TILFØJ BILLEDE</h4>
+              {this.state.files && <p>{this.state.files[0].name}</p>}
+      
+              <div className="form-group row">
+                <div className="d-flex">
+                  <label className="btn btn-outline-secondary text-nowrap m-0">
                     VÆLG FIL PÅ DIN COMPUTER
                     <input
                       className="form-control"
                       type="File"
-                      placeholder="billede"
                       name="file"
+                      onChange={this.onChange}
                       hidden
-                      ></input>
-                 </label>
-
-                 <button className="btn btn-primary align-self-center">UPLOAD</button>
-                      </div>
+                    ></input>
+                  </label>
+      
+                  <button
+                    onClick={() => this.onSubmit(uploadImage)}
+                    className="btn btn-primary align-self-center"
+                  >
+                    UPLOAD
+                  </button>
                 </div>
-                
+              </div>
             </div>
-        );
-    }
+          )}
+      </Mutation>
+    );
+  }
 }
 
 export default AddPhoto;
