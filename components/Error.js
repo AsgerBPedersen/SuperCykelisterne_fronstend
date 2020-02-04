0591ad3228a972e53e4b424808a2f6c481d1e981
TooFatToPogo <44614683+TooFatToPogo@users.noreply.github.com>
2020-02-04 09:32:55 +0100

    Error component

---
+++ components/Error.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const ErrorMessage = ({ error }) => {
+  if (!error || !error.message) return null;
+  return (
+    <p className="alert alert-danger p-2 m-1">
+      <strong>Fejl!</strong>
+      {error.message.replace("GraphQL error: ", "")}
+    </p>
+  );
+};
+
+export default ErrorMessage;
